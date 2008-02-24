718a7c584c4e994a061837ad411496f6b2aa8e4c
Daniel Rivers <me@danielrivers.com>
2008-02-24 01:32:07 +0000

    updated team

--- Tuniac1/TuniacApp/Tuniac-Meta.h
+++ Tuniac1/TuniacApp/Tuniac-Meta.h
@@ -1,3 +1,3 @@
 #pragma once
 
-#define TUNIACABOUT_CONTRIBUTERS "Mark 'noodle_awa' Turner (additional development),\nBits (bugfixes),\nThe_Ripper (the Guide, play icons)"
+#define TUNIACABOUT_CONTRIBUTERS "Mark 'noodle_awa' Turner (additional development),\nBits (bugfixes),\nDanielRivers (Features + bugfixes),\nTheRipper (the Guide, play icons)"
