25d6559bba7aba0fcabbb738f626f9b91cf019a1
tonymillion <tonymillion@164ec1c6-e1a9-4765-950d-f2db5f2bc275>
2008-03-11 12:19:36 +0000

    FLAC is another step closer to completion!

--- Tuniac1/FLAC_Plugin/FLACAudioSource.cpp
+++ Tuniac1/FLAC_Plugin/FLACAudioSource.cpp
@@ -8,3 +8,39 @@ CFLACAudioSource::CFLACAudioSource(void)
 CFLACAudioSource::~CFLACAudioSource(void)
 {
 }
+
+bool		CFLACAudioSource::Open(LPTSTR szStream)
+{
+	return false;
+}
+
+void		CFLACAudioSource::Destroy(void)
+{
+	delete this;
+}
+
+bool		CFLACAudioSource::GetLength(unsigned long * MS)
+{
+	return false;
+}
+
+bool		CFLACAudioSource::SetPosition(unsigned long * MS)
+{
+	return false;
+}
+
+bool		CFLACAudioSource::SetState(unsigned long State)
+{
+	return false;
+}
+
+bool		CFLACAudioSource::GetFormat(unsigned long * SampleRate, unsigned long * Channels)
+{
+	return false;
+}
+
+bool		CFLACAudioSource::GetBuffer(float ** ppBuffer, unsigned long * NumSamples)
+{
+	return false;
+}
+
