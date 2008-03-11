92560971a934179f40ae2441bbf4141bc7d8538e
tonymillion <tonymillion@164ec1c6-e1a9-4765-950d-f2db5f2bc275>
2008-03-11 11:39:55 +0000

    fleshed out FLAC plugin a little

--- Tuniac1/FLAC_Plugin/FLACAudioSourceSupplier.cpp
+++ Tuniac1/FLAC_Plugin/FLACAudioSourceSupplier.cpp
@@ -8,3 +8,60 @@ CFLACAudioSourceSupplier::CFLACAudioSourceSupplier(void)
 CFLACAudioSourceSupplier::~CFLACAudioSourceSupplier(void)
 {
 }
+
+void			CFLACAudioSourceSupplier::Destroy(void)
+{
+	delete this;
+}
+
+void			CFLACAudioSourceSupplier::SetHelper(IAudioSourceHelper * pHelper)
+{
+}
+
+LPTSTR			CFLACAudioSourceSupplier::GetName(void)
+{
+	return TEXT("FLAC Audio Decoder");
+}
+
+GUID			CFLACAudioSourceSupplier::GetPluginID(void)
+{
+// {AD2EB8A5-AB9A-450e-AB05-4CA1E73BF5C3}
+static const GUID FLAC_GUID = 
+{ 0xad2eb8a5, 0xab9a, 0x450e, { 0xab, 0x5, 0x4c, 0xa1, 0xe7, 0x3b, 0xf5, 0xc3 } };
+
+	return FLAC_GUID;
+}
+
+unsigned long	CFLACAudioSourceSupplier::GetFlags(void)
+{
+	return(0);
+}
+
+bool			CFLACAudioSourceSupplier::About(HWND hParent)
+{
+	return false;
+}
+bool			CFLACAudioSourceSupplier::Configure(HWND hParent)
+{
+	return false;
+}
+
+bool			CFLACAudioSourceSupplier::CanHandle(LPTSTR szSource)
+{
+	return false;
+}
+
+unsigned long	CFLACAudioSourceSupplier::GetNumCommonExts(void)
+{
+	return 1;
+}
+
+LPTSTR			CFLACAudioSourceSupplier::GetCommonExt(unsigned long ulIndex)
+{
+	return TEXT("FLAC");
+}
+
+IAudioSource *	CFLACAudioSourceSupplier::CreateAudioSource(LPTSTR szSource)
+{
+	return NULL;
+}
--- Tuniac1/FLAC_Plugin/FLAC_Plugin.cpp
+++ Tuniac1/FLAC_Plugin/FLAC_Plugin.cpp
@@ -3,4 +3,17 @@
 
 #include "stdafx.h"
 
+#include "FLACAudioSourceSupplier.h"
 
+
+extern "C" __declspec(dllexport) IAudioSourceSupplier * CreateDecoderPlugin(void)
+{
+	IAudioSourceSupplier * t = new CFLACAudioSourceSupplier;
+
+	return t;
+}
+
+extern "C" __declspec(dllexport) unsigned long		GetTuniacAudioSourceVersion(void)
+{
+	return ITUNIACAUDIOSOURCE_VERSION;
+}
