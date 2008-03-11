73ba5a4a9906759191b2826ebe2db28afaaed36c
Tony Million <tonymillion@gmail.com>
2008-03-11 20:22:05 +0000

    changed visuals Added double click to the vis window to put it in full screen mode!!!

--- Tuniac1/TuniacApp/TuniacVisual.cpp
+++ Tuniac1/TuniacApp/TuniacVisual.cpp
@@ -3,7 +3,7 @@
 
 #define TIMES	8
 
-#define DISPLAYSAMPLES	256
+#define DISPLAYSAMPLES	512
 
 typedef BOOL (APIENTRY *PFNWGLSWAPINTERVALFARPROC)( int );
 PFNWGLSWAPINTERVALFARPROC wglSwapIntervalEXT = 0;
@@ -148,6 +148,10 @@ bool	CTuniacVisual::Render(int w, int h)
 		gluOrtho2D(0.0, (float)w, (float)h, 0.0);
 		glMatrixMode(GL_MODELVIEW);
 		glLoadIdentity();
+
+		glClearColor(0.9f, 0.92f, 0.96f, 0.2f);
+		glClear (GL_COLOR_BUFFER_BIT);
+
 	}
 
 	static float AudioData[DISPLAYSAMPLES*2];
@@ -155,15 +159,15 @@ bool	CTuniacVisual::Render(int w, int h)
 
 	if(pHelper->GetVisData(AudioData, NumSamples))
 	{
-		glClearColor(0.9f, 0.92f, 0.96f, 0.2f);
-		glClear (GL_COLOR_BUFFER_BIT);
+		//glClearColor(0.9f, 0.92f, 0.96f, 0.2f);
+		//glClear (GL_COLOR_BUFFER_BIT);
 		glLoadIdentity();					// Reset The Modelview Matrix
-		//glColor4f(0.9f, 0.92f, 0.96f, 0.4f);
-		//glRectf(0,0,m_LastWidth, m_LastHeight);
+		glColor4f(0.9f, 0.92f, 0.96f, 0.5f);
+		glRectf(0,0,m_LastWidth, m_LastHeight);
 
 
 		// draw background grid
-		glColor4f(0,0,0, 0.2f);
+		glColor4f(0,0,0, 0.1f);
 		glBegin(GL_LINES);
 			for(unsigned long x=0; x<m_LastWidth; x+=64)
 			{
@@ -185,30 +189,30 @@ bool	CTuniacVisual::Render(int w, int h)
 			float halfheight = ((float)m_LastHeight / 2.0f);
 			float multiplier = (float)m_LastWidth / (float)(DISPLAYSAMPLES/2.0f);
 
-			glColor4f(0,0,0, 0.6f);
+			glColor4f(0,0,0, 0.5f);
 			glBegin(GL_QUADS);
 			{
 				for(unsigned int samp=0; samp<NumSamples; samp++)
 				{
-					glVertex2f(samp*multiplier,	(halfheight - (halfheight/2)));
-					glVertex2f(samp*multiplier,	(halfheight - (halfheight/2))		+ (AudioData[samp*2]		* halfheight));
+					glVertex2f(samp*multiplier,		halfheight);
+					glVertex2f(samp*multiplier,		halfheight 	- abs(AudioData[samp*2]		* halfheight) );
 
-					glVertex2f((samp+1)*multiplier,	(halfheight - (halfheight/2))	+ (AudioData[samp*2]		* halfheight));
-					glVertex2f((samp+1)*multiplier,	(halfheight - (halfheight/2)));
+					glVertex2f((samp+1)*multiplier,	halfheight 	- abs(AudioData[samp*2]		* halfheight));
+					glVertex2f((samp+1)*multiplier,	halfheight);
 				}
 			}
 			glEnd();
 
-			glColor4f(0,0,0, 0.6f);
+			glColor4f(0,0,0, 0.5f);
 			glBegin(GL_QUADS);
 			{
 				for(unsigned int samp=0; samp<NumSamples; samp++)
 				{
-					glVertex2f(samp*multiplier,	(halfheight + (halfheight/2)));
-					glVertex2f(samp*multiplier,	(halfheight + (halfheight/2))		+ (AudioData[(samp*2)+1]		* halfheight));
+					glVertex2f(samp*multiplier,		halfheight);
+					glVertex2f(samp*multiplier,		halfheight + abs(AudioData[(samp*2)+1]		* halfheight));
 
-					glVertex2f((samp+1)*multiplier,	(halfheight + (halfheight/2))	+ (AudioData[(samp*2)+1]		* halfheight));
-					glVertex2f((samp+1)*multiplier,	(halfheight + (halfheight/2)));
+					glVertex2f((samp+1)*multiplier,	halfheight + abs(AudioData[(samp*2)+1]		* halfheight));
+					glVertex2f((samp+1)*multiplier,	halfheight);
 				}
 			}
 			glEnd();
