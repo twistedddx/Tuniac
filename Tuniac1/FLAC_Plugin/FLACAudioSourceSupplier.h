358a32833987bf4c40c8525864027edce348a983
Tony Million <tonymillion@gmail.com>
2008-03-11 11:39:55 +0000

    fleshed out FLAC plugin a little

--- Tuniac1/FLAC_Plugin/FLACAudioSourceSupplier.h
+++ Tuniac1/FLAC_Plugin/FLACAudioSourceSupplier.h
@@ -9,4 +9,22 @@ class CFLACAudioSourceSupplier :
 public:
 	CFLACAudioSourceSupplier(void);
 	~CFLACAudioSourceSupplier(void);
+
+public:
+	void			Destroy(void);
+
+	void			SetHelper(IAudioSourceHelper * pHelper);
+
+	LPTSTR			GetName(void);
+	GUID			GetPluginID(void);
+	unsigned long	GetFlags(void);
+
+	bool			About(HWND hParent);
+	bool			Configure(HWND hParent);
+
+	bool			CanHandle(LPTSTR szSource);
+	unsigned long	GetNumCommonExts(void);
+	LPTSTR			GetCommonExt(unsigned long ulIndex);
+
+	IAudioSource *	CreateAudioSource(LPTSTR szSource);
 };
