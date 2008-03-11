e74c964f29a611bd800cfdda60136b3022b22308
tonymillion <tonymillion@164ec1c6-e1a9-4765-950d-f2db5f2bc275>
2008-03-11 11:30:09 +0000

    added more FLAC stuff

---
+++ Tuniac1/FLAC_Plugin/FLACAudioSource.cpp
@@ -0,0 +1,10 @@
+#include "StdAfx.h"
+#include "FLACAudioSource.h"
+
+CFLACAudioSource::CFLACAudioSource(void)
+{
+}
+
+CFLACAudioSource::~CFLACAudioSource(void)
+{
+}
---
+++ Tuniac1/FLAC_Plugin/FLACAudioSource.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "iaudiosource.h"
+
+class CFLACAudioSource :
+	public IAudioSource
+{
+public:
+	CFLACAudioSource(void);
+	~CFLACAudioSource(void);
+};
