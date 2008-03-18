4e2a6d1350be9b3f32e5b4304c5e1390b9805d51
twistedddx <twistedddx@164ec1c6-e1a9-4765-950d-f2db5f2bc275>
2008-03-18 12:53:14 +0000

    Wavpack support

--- Tuniac1/wv_Plugin/WVDecoder.cpp
+++ Tuniac1/wv_Plugin/WVDecoder.cpp
@@ -1,7 +1,7 @@
 #include "StdAfx.h"
 #include "wvdecoder.h"
 
-#define NUM_SAMPLES_UNPACK 576L
+#define NUM_SAMPLES_UNPACK 4096L
 
 CWVDecoder::CWVDecoder(void) : 
 	m_bFloatMode(false)
@@ -18,7 +18,7 @@ bool CWVDecoder::Open(LPTSTR szSource)
 	WideCharToMultiByte(CP_UTF8, 0, szSource, -1, tempname, _MAX_PATH, 0, 0);
 
     char error [128];
-    wpc = WavpackOpenFileInput(tempname, error, OPEN_WVC | OPEN_NORMALIZE, 23);
+    wpc = WavpackOpenFileInput(tempname, error, OPEN_WVC, 23);
     if (!wpc)
         return(false);
 
@@ -28,8 +28,6 @@ bool CWVDecoder::Open(LPTSTR szSource)
 	ulBitsPerSample = WavpackGetBitsPerSample(wpc);
 	ulBytesPerSample = WavpackGetBytesPerSample(wpc);
 
-	pRawData = (long*)malloc(NUM_SAMPLES_UNPACK*ulChannels*4);
-
 	if(WavpackGetMode(wpc) & MODE_FLOAT)
 		m_bFloatMode = true;
 
@@ -38,7 +36,6 @@ bool CWVDecoder::Open(LPTSTR szSource)
 
 bool CWVDecoder::Close()
 {
-	free(pRawData);
 	wpc = WavpackCloseFile(wpc);
 	wpc = NULL;
 	return(true);
@@ -68,10 +65,8 @@ bool		CWVDecoder::GetLength(unsigned long * MS)
 
 bool		CWVDecoder::SetPosition(unsigned long * MS)
 {
-	double pos = *MS;
-	WavpackSeekSample(wpc, (int)(ulSampleRate / 1000 * pos));
-
-	return(false);
+	WavpackSeekSample(wpc, (int)(ulSampleRate / 1000 * (unsigned long)*MS));
+	return(true);
 }
 
 bool		CWVDecoder::SetState(unsigned long State)
@@ -83,17 +78,17 @@ bool		CWVDecoder::GetBuffer(float ** ppBuffer, unsigned long * NumSamples)
 {
 	*NumSamples =0;
 
-	unsigned status = WavpackUnpackSamples(wpc, (int32_t *)pRawData, NUM_SAMPLES_UNPACK);
+	unsigned status = WavpackUnpackSamples(wpc, (int32_t *)m_RawData, (NUM_SAMPLES_UNPACK / ulChannels));
     if (!status)
         return false;
 
 	if(m_bFloatMode)
 	{
-		*ppBuffer = (float*)pRawData;
+		*ppBuffer = (float*)m_RawData;
 	}
 	else
 	{
-		int * pData = (int *)pRawData;
+		int * pData = (int *)m_RawData;
 		float * pBuffer = m_Buffer;
 		
 		float divider = 0.0;
@@ -102,9 +97,17 @@ bool		CWVDecoder::GetBuffer(float ** ppBuffer, unsigned long * NumSamples)
 		{
 			divider = 128.0f;
 		}
+		else if(ulBitsPerSample == 12)
+		{
+			divider = 4095.0f;
+		}
 		else if(ulBitsPerSample == 16)
 		{
 			divider = 32767.0f;
+		}
+		else if(ulBitsPerSample == 20)
+		{
+			divider = 1048575.0f;
 		}
 		else if(ulBitsPerSample == 24)
 		{
