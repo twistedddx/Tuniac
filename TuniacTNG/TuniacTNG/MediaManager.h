2fe777862665751909ac244b918f9d231ea31342
Tony Million <tonymillion@gmail.com>
2008-03-31 16:16:18 +0000

    updates to medialibrary

--- TuniacTNG/TuniacTNG/MediaManager.h
+++ TuniacTNG/TuniacTNG/MediaManager.h
@@ -3,6 +3,40 @@
 #include <IInfoHandler.h>
 #include <Singleton.h>
 
+typedef struct _MEDIAITEM_
+{
+	unsigned __int64		ulItemID;
+
+	SYSTEMTIME				dateAdded;
+	unsigned long			fileOnline;
+
+	String					filename;
+	unsigned long			ulFilesize;
+
+	String					title;	
+	String					artist;
+	String					album;
+	String					albumartist;
+	String					composer;
+	String					genre;
+	String					comment;
+
+	unsigned long			ulTrack;
+	unsigned long			ulMaxTrack;
+
+	unsigned long			ulDisk;
+	unsigned long			ulMaxDisk;
+
+	unsigned long			ulRating;
+	unsigned __int64		ulBPM;
+
+	unsigned long			ulPlayTimeMS;
+
+	unsigned long			ulSampleRate;
+	unsigned long			ulChannelCount;
+	unsigned long			ulBitRate;
+} MediaItem;
+
 class CMediaManager : public CSingleton<CMediaManager>
 {
 protected:
@@ -13,6 +47,8 @@ protected:
 	} InfoHandler;
 
 	std::vector<InfoHandler>		m_vInfoHandlers;
+
+	bool PopulateMediaItemFromAccessor(MediaItem & pItem, IInfoAccessor * pAccessor);
 
 public:
 	CMediaManager(void);
