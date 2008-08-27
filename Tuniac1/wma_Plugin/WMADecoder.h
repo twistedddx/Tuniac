73310fff590776ad33315aeee1a0dc89a3a4b0db
twistedddx <twistedddx@users.noreply.github.com>
2008-08-27 13:37:54 +0000

    Start of WMA plugin

---
+++ Tuniac1/wma_Plugin/WMADecoder.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include "iaudiosource.h"
+
+#include <wmsdk.h>
+
+
+#ifndef SAFE_RELEASE
+#define SAFE_RELEASE( x )  \
+    if ( NULL != x )       \
+    {                      \
+        x->Release( );     \
+        x = NULL;          \
+    }
+
+#endif
+
+#ifndef SAFE_ARRAYDELETE
+#define SAFE_ARRAYDELETE( x )  \
+    if( NULL != x )        \
+    {                      \
+        delete [] x;       \
+        x = NULL;          \
+    }
+#endif
+
+class CWMADecoder :
+	public IAudioSource
+{
+protected:
+
+	float				m_Buffer[131072];
+	DWORD				m_audioBufferLength;
+
+	IWMSyncReader			*	m_ISyncReader;
+	INSSBuffer				*	m_pINSSBuffer;
+	IWMOutputMediaProps		*	m_IAudioOutputProps;
+	WM_MEDIA_TYPE			*	m_theMediaType;
+	WORD						m_iAudioStreamNumber;
+	DWORD						m_theOutputsCount;
+	HRESULT hr;
+
+	int m_iAudioOutputNumber;
+
+	unsigned long ulChannels;
+	unsigned long ulSampleRate;
+	unsigned long m_qwTotalTimeInMS;
+
+public:
+	CWMADecoder(void);
+	~CWMADecoder(void);
+
+	bool Open(LPTSTR szSource);
+	bool Close();
+
+public:
+	void		Destroy(void);
+	bool		GetFormat(unsigned long * SampleRate, unsigned long * Channels);
+	bool		GetLength(unsigned long * MS);
+	bool		SetPosition(unsigned long * MS);
+	bool		SetState(unsigned long State);
+	bool		GetBuffer(float ** ppBuffer, unsigned long * NumSamples);
+};
---
+++ Tuniac1/wma_Plugin/WMADecoderPlugin.cpp
@@ -0,0 +1,83 @@
+#include "StdAfx.h"
+#include "wmadecoderplugin.h"
+#include "WMADecoder.h"
+
+CWMADecoderPlugin::CWMADecoderPlugin(void)
+{
+}
+
+CWMADecoderPlugin::~CWMADecoderPlugin(void)
+{
+}
+
+void			CWMADecoderPlugin::Destroy(void)
+{
+	delete this;
+}
+
+void			CWMADecoderPlugin::SetHelper(IAudioSourceHelper * pHelper)
+{
+}
+
+LPTSTR			CWMADecoderPlugin::GetName(void)
+{
+	return(TEXT("WMA Decoder"));
+}
+
+GUID			CWMADecoderPlugin::GetPluginID(void)
+{
+	static const GUID mpcPluginGUID = {0x65d0eaf3, 0x21f8, 0x4db9, {0xb1, 0x61, 0x28, 0x5d, 0xb6, 0xe1, 0xe2, 0x66} };
+	return(mpcPluginGUID);
+}
+
+unsigned long	CWMADecoderPlugin::GetFlags(void)
+{
+	return FLAGS_ABOUT;
+}
+
+bool			CWMADecoderPlugin::About(HWND hParent)
+{
+    MessageBox(hParent, TEXT("WMA Plugin \nUsing FFDShow based libwma "), GetName(), MB_OK | MB_ICONINFORMATION);
+	return true;
+}
+
+bool			CWMADecoderPlugin::Configure(HWND hParent)
+{
+	return(false);
+}
+
+bool			CWMADecoderPlugin::CanHandle(LPTSTR szSource)
+{
+	LPTSTR	t = &szSource[lstrlen(szSource)-4];
+	if(!lstrcmpi(TEXT(".wma"), t))
+	{
+		return(true);
+	}
+
+	return(false);
+}
+
+
+unsigned long	CWMADecoderPlugin::GetNumCommonExts(void)
+{
+	return 1;
+}
+
+LPTSTR			CWMADecoderPlugin::GetCommonExt(unsigned long ulIndex)
+{
+	return TEXT(".wma");
+}
+
+
+IAudioSource *		CWMADecoderPlugin::CreateAudioSource(LPTSTR szSource, IAudioFileIO * pFileIO)
+{
+	CWMADecoder *	pDec = new CWMADecoder();
+
+	if(!pDec->Open(szSource))
+	{
+		delete pDec;
+		return(NULL);
+	}
+
+	return (IAudioSource *)pDec;
+}
