f1e23ba288dd0d371fb39b87898fcddc4196300c
twistedddx <twistedddx@164ec1c6-e1a9-4765-950d-f2db5f2bc275>
2008-08-28 15:16:21 +0000

    wav mmio switch

--- Tuniac1/wav_Plugin/WAVDecoder.cpp
+++ Tuniac1/wav_Plugin/WAVDecoder.cpp
@@ -2,14 +2,6 @@
 
 #include "wavdecoder.h"
 
-bool CWAVDecoder::FOURCC_EQUAL(char* X, char* Y)
-{
-	if(X[0]==Y[0] && X[1]==Y[1] && X[2]==Y[2] && X[3]==Y[3])
-		return true;
-
-	return false;
-}
-
 CWAVDecoder::CWAVDecoder(void)
 {
 }
@@ -20,113 +12,78 @@ CWAVDecoder::~CWAVDecoder(void)
 
 bool CWAVDecoder::Open(LPTSTR szSource)
 {
-    m_file = _wfopen(szSource,TEXT("rbS"));
-    if (m_file)
-    {
-		read = fread(&wav,sizeof(WAV_HDR),1,m_file);
+	hWav = mmioOpen( szSource, NULL, MMIO_ALLOCBUF | MMIO_READ );
 
-		//check for RIFF header
-		if(!FOURCC_EQUAL(wav.rID, "RIFF"))
-			return false;
+	if (hWav==NULL)
+		return false;
 
-		//check for WAVE header
-		if(!FOURCC_EQUAL(wav.wID, "WAVE"))
-			return false;
+	parent.fccType = mmioFOURCC('W', 'A', 'V', 'E');
+	if (mmioDescend(hWav, &parent, NULL, MMIO_FINDRIFF))
+		return false;
 
-		//check for fmt header
-		if(!FOURCC_EQUAL(wav.fId, "fmt "))
-			return false;
+	child.ckid = mmioFOURCC('f', 'm', 't', ' ');
+	if (mmioDescend(hWav, &child,  &parent, 0))
+		return false;
 
-		m_bFloatMode = 0;
-		if(wav.wFormatTag == 3)
-			m_bFloatMode = 1;
-		else if(wav.wFormatTag!=1)
-			//unsupported format
-			return false;
+	if (mmioRead(hWav, (char*)&wfmex, sizeof(WAVEFORMATEX)) != sizeof(WAVEFORMATEX))
+		return false;
 
-		rmore = wav.pcm_header_len - (sizeof(WAV_HDR) - 20);
-		read = fseek(m_file,rmore,SEEK_CUR);
-		if(read!=0)
-			//cant seek
-			return false;
-	}
+	if (mmioAscend(hWav, &child, 0))
+		return false;
 
+	child.ckid = mmioFOURCC('d', 'a', 't', 'a');
+	if (mmioDescend(hWav, &child, &parent, MMIO_FINDCHUNK))
+		return false;
 
-	// read chunks until a 'data' chunk is found
-	sflag = 1;
-	while(sflag!=0){
+	ulChunkSize = child.cksize;
 
-		// check attempts before it gets out of hand
-		if(sflag>10)
-			//too many chunks
-			return false;
-
-		 // read chunk header
-		read = fread(&chk,sizeof(CHUNK_HDR),(size_t)1,m_file);
-		if(read!=1)
-			//cant read chunk
-			return false;
-
-		// check chunk type
-			if(FOURCC_EQUAL(chk.dId, "data"))
-				break;
-	 
-		// skip over chunk
-		sflag++;
-		read = fseek(m_file,chk.dLen,SEEK_CUR);
-		if(read!=0)
-			//cant seek
-			return false;
-	}
-
-	if(wav.nBitsPerSample == 8)
+	if(wfmex.wBitsPerSample == 8)
 		m_divider = 128.0f;
-	else if(wav.nBitsPerSample == 16)
+	else if(wfmex.wBitsPerSample == 16)
 		m_divider = 32767.0f;
-	else if(wav.nBitsPerSample == 24)
+	else if(wfmex.wBitsPerSample == 24)
 		m_divider = 8388608.0f;
-	else if(wav.nBitsPerSample == 32)
+	else if(wfmex.wBitsPerSample == 32)
 		m_divider = 2147483648.0;
 
-	total_read = 0;
 
 	return(true);
 }
 
 bool CWAVDecoder::Close()
 {
+	mmioClose(hWav, 0);
 	return(true);
 }
 
 void		CWAVDecoder::Destroy(void)
 {
-	fclose(m_file);
 	Close();
 	delete this;
 }
 
 bool		CWAVDecoder::GetFormat(unsigned long * SampleRate, unsigned long * Channels)
 {
-	*SampleRate = wav.nSamplesPerSec;
-	*Channels	= wav.nChannels;
+	*SampleRate = wfmex.nSamplesPerSec;
+	*Channels	= wfmex.nChannels;
 
 	return(true);
 }
 
 bool		CWAVDecoder::GetLength(unsigned long * MS)
 {
-	*MS = 1000 * ((unsigned long)chk.dLen / (unsigned long)wav.nAvgBytesPerSec);
+	*MS = 1000 * ((unsigned long)ulChunkSize / (unsigned long)wfmex.nAvgBytesPerSec);
 	return(true);
 }
 
 bool		CWAVDecoder::SetPosition(unsigned long * MS)
 {
 
-	unsigned long ByteOffset = ((float)*MS / 1000.0f) * (unsigned long)wav.nAvgBytesPerSec;
-	ByteOffset -= ByteOffset % wav.nBlockAlign;
+	unsigned long ByteOffset = ((float)*MS / 1000.0f) * (unsigned long)wfmex.nAvgBytesPerSec;
+	ByteOffset -= ByteOffset % wfmex.nBlockAlign;
 
 	//seek in to where we want
-	fseek(m_file,wav.pcm_header_len+ByteOffset,SEEK_SET);
+	mmioSeek(hWav, parent.cksize+ByteOffset, SEEK_SET);
 	return(true);
 }
 
@@ -137,38 +94,21 @@ bool		CWAVDecoder::SetState(unsigned long State)
 
 bool		CWAVDecoder::GetBuffer(float ** ppBuffer, unsigned long * NumSamples)
 {
+	*NumSamples = 0;
+
 	char			buffer[BUF_SIZE] = {0};
-	read = 0;
-
-	/* read signal data */
-	read = fread(buffer,sizeof(char),BUF_SIZE,m_file);
-	if(read > 0)
+	Read = mmioRead(hWav, (char*)buffer, BUF_SIZE);
+	if(Read > 0)
 	{
-		if(m_bFloatMode)
+ 		short * pData = (short*)buffer;
+		for(int x=0; x<(Read/wfmex.nChannels); x++)
 		{
-			*ppBuffer = (float*)buffer;
+			m_Buffer[x] = (float)pData[x] / m_divider;
 		}
-		else
-		{
-     		short * pData = (short*)buffer;
-			for(int x=0; x<(read/wav.nChannels); x++)
-			{
-				m_Buffer[x] = (float)pData[x] / m_divider;
-			}
-			*ppBuffer = m_Buffer;
-		}
+		*ppBuffer = m_Buffer;
 
-		*NumSamples = (read/wav.nChannels);
+		*NumSamples = (Read/wfmex.nChannels);
+
 	}
-	else
-		//cant read chunk
-		return false;
-
-	total_read += read;
-
-	if(total_read >= chk.dLen){
-	  return false;
-	}
-
 	return(true);
 }
