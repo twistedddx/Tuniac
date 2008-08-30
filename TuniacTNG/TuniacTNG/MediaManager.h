3f9beead1dc86c75f188bc05d14c56d0de0372d9
tonymillion <tonymillion@164ec1c6-e1a9-4765-950d-f2db5f2bc275>
2008-08-30 17:36:22 +0000

    updates to TNG and also removed ability to sort playlists because I want it like that

--- TuniacTNG/TuniacTNG/MediaManager.h
+++ TuniacTNG/TuniacTNG/MediaManager.h
@@ -24,7 +24,7 @@ protected:
 	bool InsertItemToMediaLibraryUsingConnection(sqlite3x::sqlite3_connection & con, MediaItem & pItem);
 	bool IsFileInLibrary(sqlite3x::sqlite3_connection & con, String filename);
 
-	bool ReaderMediaItem(sqlite3x::sqlite3_cursor & cursor, MediaItem & item);
+	bool CursorToMediaItem(sqlite3x::sqlite3_cursor & cursor, MediaItem & item);
 
 	bool RebuildIDList(void);
 	void CreateDatabaseSchema(sqlite3x::sqlite3_connection & con);
