4de441a637797ac914ffc68b475709a69f3fd532
Tony Million <tonymillion@gmail.com>
2008-10-23 12:37:12 +0000

    added extra threading code, needs more testing

--- Tuniac1/TuniacApp/IAudioCallback.h
+++ Tuniac1/TuniacApp/IAudioCallback.h
@@ -25,6 +25,6 @@ class IAudioCallback
 {
 public:
 	virtual bool GetBuffer(float * pAudioBuffer, unsigned long NumSamples) = 0;
-	virtual bool ServiceStream(void) = 0;			// new so audio playback thread can produce a new buffer while its not doing anything else!!!
+	//virtual bool ServiceStream(void) = 0;			// new so audio playback thread can produce a new buffer while its not doing anything else!!!
 
 };
