c25a58662918f19123d7200042fa9707f5953e47
twistedddx <twistedddx@164ec1c6-e1a9-4765-950d-f2db5f2bc275>
2011-04-21 07:48:07 +0000

    Change jpeg library to jpeglib 8c. Fix b&w jpeg decoding. Change scaling method to HALFTONE.

--- Tuniac1/TuniacApp/AlbumArt.h
+++ Tuniac1/TuniacApp/AlbumArt.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "../jpegdecoder/jpegdecoder.h"
+#include "jpeglib.h"
 
 class CAlbumArt
 {
@@ -10,12 +10,8 @@ protected:
 	LPVOID					m_pBitmapData;
 	unsigned long			m_ulBitmapWidth;
 	unsigned long			m_ulBitmapHeight;
-	unsigned long			m_ulBytesPerPixel;
-	unsigned long			m_ulComponents;
 
 	TCHAR					szCurrentArtSource[512];
-
-	bool	LoadJpegData(jpeg_decoder_stream & input_stream);
 
 public:
 	CAlbumArt(void);
@@ -24,7 +20,7 @@ public:
 	LPTSTR	GetCurrentArtSource(void);
 	void	SetCurrentArtSource(LPTSTR szNewArtSource);
 
-	bool	SetSource(LPVOID pData, unsigned long ulDataLength, LPTSTR szMimeType);
+	bool	SetSource(LPVOID pCompressedData, unsigned long ulDataLength, LPTSTR szMimeType);
 	bool	SetSource(LPTSTR szFilename);
 
 	bool	Draw(HDC hDC, long x, long y, long lWidth, long lHeight);
