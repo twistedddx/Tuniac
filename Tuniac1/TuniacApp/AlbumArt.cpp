7c890e00c431795bd4d4235d73f402a62760faf2
twistedddx <twistedddx@users.noreply.github.com>
2011-04-21 07:48:07 +0000

    Change jpeg library to jpeglib 8c. Fix b&w jpeg decoding. Change scaling method to HALFTONE.

--- Tuniac1/TuniacApp/AlbumArt.cpp
+++ Tuniac1/TuniacApp/AlbumArt.cpp
@@ -13,65 +13,8 @@ extern unsigned decodePNG(	std::vector<unsigned char>& out_image_32bit,
 							const unsigned char* in_png, 
 							unsigned in_size);
 
-
-class jpeg_decoder_memory_stream : public jpeg_decoder_stream
+CAlbumArt::CAlbumArt(void)
 {
-  LPVOID			m_jPegData;
-  unsigned long		m_Size;
-  unsigned long		m_Offset;
-  bool eof_flag, error_flag;
-
-public:
-
-  jpeg_decoder_memory_stream(LPVOID pMemory, unsigned long ulSize)
-  {
-    m_jPegData	= pMemory;
-	m_Size		= ulSize;
-	m_Offset	= 0;
-    eof_flag	= false;
-    error_flag	= false;
-  }
-
-  virtual ~jpeg_decoder_memory_stream()
-  {
-  }
- 
-  virtual int read(uchar *Pbuf, int max_bytes_to_read, bool *Peof_flag)
-  {
-		if (eof_flag)
-		{
-		  *Peof_flag = true;
-		  return (0);
-		}
-
-		if (error_flag)
-			return (-1);
-
-		unsigned long bytesthistime = min(max_bytes_to_read, m_Size - m_Offset);
-
-		CopyMemory(Pbuf, ((unsigned char*)m_jPegData)+m_Offset, bytesthistime);
-		m_Offset += bytesthistime;
-
-		if(m_Offset >= m_Size)
-		{
-			eof_flag = true;
-			*Peof_flag = true;
-		}
-
-		return (bytesthistime);
-	}
-};
-
-CAlbumArt::CAlbumArt(void) :
-	m_pBitmapData(NULL)
-{
-	m_ulBitmapWidth		= 100;
-	m_ulBitmapHeight	= 100;
-	m_ulBytesPerPixel	= 4;
-
-	m_pBitmapData = malloc(m_ulBitmapWidth * m_ulBitmapHeight * m_ulBytesPerPixel);
-	ZeroMemory(m_pBitmapData, m_ulBitmapWidth * m_ulBitmapHeight * m_ulBytesPerPixel);
-
 }
 
 CAlbumArt::~CAlbumArt(void)
@@ -82,63 +25,6 @@ CAlbumArt::~CAlbumArt(void)
 		m_pBitmapData = NULL;
 	}
 
-}
-
-bool	CAlbumArt::LoadJpegData(jpeg_decoder_stream & input_stream)
-{
-	//even with use_mmx set to true, under x64 it will be ignored and set to false due to asm requirement
-	jpeg_decoder d(&input_stream, false);
-	if(d.get_error_code() != 0)
-	{
-		// Always be sure to delete the input stream object _after_
-		// the decoder is deleted. Reason: the decoder object calls the input
-		// stream's detach() method.
-		return (false);
-	}
-
-	if(d.begin())
-		return false;
-
-	m_ulBitmapWidth		= d.get_width();
-	m_ulBitmapHeight	= d.get_height();
-	m_ulBytesPerPixel	= d.get_bytes_per_pixel();
-	m_ulComponents		= d.get_num_components();
-
-	if(m_pBitmapData)
-	{
-		free(m_pBitmapData);
-		m_pBitmapData = NULL;
-	}
-
-	m_pBitmapData = malloc(m_ulBitmapWidth * m_ulBitmapHeight * m_ulBytesPerPixel);
-	if(!m_pBitmapData)
-		return false;
-
-	unsigned char * pData = (unsigned char *)m_pBitmapData;
-	while(true)
-	{
-		unsigned char * Pscan_line_ofs;
-		uint scan_line_len;
-
-		int src_bpp = d.get_bytes_per_pixel();
-
-
-		if(d.decode((void**)&Pscan_line_ofs, &scan_line_len))
-			break;
-
-		for(int x=0; x<scan_line_len; x+= src_bpp)
-		{
-			pData[x+0]	= Pscan_line_ofs[x+2];
-			pData[x+1]	= Pscan_line_ofs[x+1];
-			pData[x+2]	= Pscan_line_ofs[x+0];
-			pData[x+3]	= Pscan_line_ofs[x+3];
-		}
-
-		pData += m_ulBitmapWidth * m_ulBytesPerPixel;
-		int x=0;
-	}
-
-	return true;
 }
 
 void CAlbumArt::SetCurrentArtSource(LPTSTR szNewArtSource)
@@ -154,37 +40,93 @@ LPTSTR CAlbumArt::GetCurrentArtSource(void)
 	return szCurrentArtSource;
 }
 
-bool CAlbumArt::SetSource(LPVOID pData, unsigned long ulDataLength, LPTSTR szMimeType)
+bool CAlbumArt::SetSource(LPVOID pCompressedData, unsigned long ulDataLength, LPTSTR szMimeType)
 {
 	CAutoLock lock(&m_ArtLock);
 
 	if(StrStrI(szMimeType, TEXT("image/jpeg")) || StrStrI(szMimeType, TEXT("image/jpg")))
 	{
-		jpeg_decoder_memory_stream	input_stream(pData, ulDataLength);
+		struct jpeg_decompress_struct cinfo;
+		struct jpeg_error_mgr jerr;
+		cinfo.err = jpeg_std_error(&jerr);
+		jpeg_create_decompress(&cinfo);
+		jpeg_mem_src(&cinfo, (unsigned char *)pCompressedData, ulDataLength);
+		jpeg_read_header(&cinfo, TRUE);
+		jpeg_start_decompress(&cinfo);
 
-		if(!LoadJpegData(input_stream))
+		m_ulBitmapWidth		= cinfo.output_width;
+		m_ulBitmapHeight	= cinfo.output_height;
+
+		if(m_pBitmapData)
+		{
+			free(m_pBitmapData);
+			m_pBitmapData = NULL;
+		}
+
+		m_pBitmapData = malloc(m_ulBitmapWidth * m_ulBitmapHeight * 4);
+		if(!m_pBitmapData)
 			return false;
 
+		unsigned char * pOutData = (unsigned char *)m_pBitmapData;
+
+		JSAMPLE *buffer;
+		int row_stride = cinfo.output_width * cinfo.output_components;
+		if ((buffer = (JSAMPLE *)malloc(row_stride * sizeof(JSAMPLE))) == NULL) 
+			return false;
+
+
+		while (cinfo.output_scanline < cinfo.output_height)
+		{
+			jpeg_read_scanlines(&cinfo,&buffer,1);
+			if (cinfo.output_components == 3)
+			{
+				int y = 0;
+				for (int i=0; i<row_stride; i+=3)
+				{
+					pOutData[y] = buffer[i+2];
+					pOutData[y+1] = buffer[i+1];
+					pOutData[y+2] = buffer[i];
+					pOutData[y+3] = 255;
+					y += 4;
+				}
+			}
+			else
+			{
+				int y = 0;
+				for (int i=0; i<row_stride; i++)
+				{
+					pOutData[y] = buffer[i];
+					pOutData[y+1] = buffer[i];
+					pOutData[y+2] = buffer[i];
+					pOutData[y+3] = 255;
+					y += 4;
+				}
+			}
+			pOutData += m_ulBitmapWidth * 4;
+		}
+
+		jpeg_finish_decompress(&cinfo);
+		jpeg_destroy_decompress(&cinfo);
+		free(buffer);
+
 		return true;
+
 	}
 
 	if( StrStrI(szMimeType, TEXT("image/png")) )
 	{
-		//TODO: PNG HERE PLSKTHX
-		std::vector<unsigned char> imageData;
+		std::vector<unsigned char> decompressedData;
 
 		// fills out our std::vector as a 32bit ARGB data stream
 		unsigned int w, h;
-		if(decodePNG(	imageData,
+		if(decodePNG(	decompressedData,
 					w,
 					h,
-					(const unsigned char *)pData,
+					(const unsigned char *)pCompressedData,
 					ulDataLength) == 0)
 		{
 			m_ulBitmapWidth		= w;
 			m_ulBitmapHeight	= h;
-			m_ulBytesPerPixel	= 4;
-			m_ulComponents		= 4;
 
 			if(m_pBitmapData)
 			{
@@ -192,19 +134,19 @@ bool CAlbumArt::SetSource(LPVOID pData, unsigned long ulDataLength, LPTSTR szMim
 				m_pBitmapData = NULL;
 			}
 
-			m_pBitmapData = malloc(m_ulBitmapWidth * m_ulBitmapHeight * m_ulBytesPerPixel);
+			m_pBitmapData = malloc(m_ulBitmapWidth * m_ulBitmapHeight * 4);
 			if(!m_pBitmapData)
 				return false;
 
 			//CopyMemory(m_pBitmapData, &imageData[0], imageData.size());
-			unsigned char * pData	= (unsigned char *)m_pBitmapData;
-			unsigned char * pSrc	= (unsigned char *)&imageData[0];
-			for(int x=0; x<imageData.size(); x+= m_ulBytesPerPixel)
+			unsigned char * pOutData	= (unsigned char *)m_pBitmapData;
+			unsigned char * pSrc	= (unsigned char *)&decompressedData[0];
+			for(int x=0; x<decompressedData.size(); x+= 4)
 			{
-				pData[x+0]	= pSrc[x+2];
-				pData[x+1]	= pSrc[x+1];
-				pData[x+2]	= pSrc[x+0];
-				pData[x+3]	= pSrc[x+3];
+				pOutData[x]		= pSrc[x+2];
+				pOutData[x+1]	= pSrc[x+1];
+				pOutData[x+2]	= pSrc[x];
+				pOutData[x+3]	= pSrc[x+3];
 			}
 
 			return true;
@@ -222,24 +164,80 @@ bool CAlbumArt::SetSource(LPTSTR szFilename)
 
 	if(StrStrI(szFilename, TEXT(".jpeg")) || StrStrI(szFilename, TEXT(".jpg")) || StrStrI(szFilename, TEXT(".jpe")))
 	{
-		bool bRet = false;
+		FILE * imageFile = _wfopen(szFilename, TEXT("rbS"));
+		if(imageFile == NULL)
+			return false;
 
-		jpeg_decoder_file_stream input_stream;
-		if (input_stream.open(szFilename))
+		struct jpeg_decompress_struct cinfo;
+		struct jpeg_error_mgr jerr;
+		cinfo.err = jpeg_std_error(&jerr);
+		jpeg_create_decompress(&cinfo);
+		jpeg_stdio_src(&cinfo, imageFile);
+		jpeg_read_header(&cinfo, TRUE);
+		jpeg_start_decompress(&cinfo);
+
+		m_ulBitmapWidth		= cinfo.output_width;
+		m_ulBitmapHeight	= cinfo.output_height;
+
+		if(m_pBitmapData)
 		{
-			return (false);
+			free(m_pBitmapData);
+			m_pBitmapData = NULL;
 		}
 
-		bRet = LoadJpegData(input_stream);
+		m_pBitmapData = malloc(m_ulBitmapWidth * m_ulBitmapHeight * 4);
+		if(!m_pBitmapData)
+			return false;
 
-		input_stream.close();
+		unsigned char * pOutData = (unsigned char *)m_pBitmapData;
 
-		return bRet;
+		JSAMPLE *buffer;
+		int row_stride = cinfo.output_width * cinfo.output_components;
+		if ((buffer = (JSAMPLE *)malloc(row_stride * sizeof(JSAMPLE))) == NULL) 
+			return false;
+
+
+		while (cinfo.output_scanline < cinfo.output_height)
+		{
+			jpeg_read_scanlines(&cinfo,&buffer,1);
+			if (cinfo.output_components == 3)
+			{
+				int y = 0;
+				for (int i=0; i<row_stride; i+=3)
+				{
+					pOutData[y] = buffer[i+2];
+					pOutData[y+1] = buffer[i+1];
+					pOutData[y+2] = buffer[i];
+					pOutData[y+3] = 255;
+					y += 4;
+				}
+			}
+			else
+			{
+				int y = 0;
+				for (int i=0; i<row_stride; i++)
+				{
+					pOutData[y] = buffer[i];
+					pOutData[y+1] = buffer[i];
+					pOutData[y+2] = buffer[i];
+					pOutData[y+3] = 255;
+					y += 4;
+				}
+			}
+			pOutData += m_ulBitmapWidth * 4;
+		}
+
+		jpeg_finish_decompress(&cinfo);
+		jpeg_destroy_decompress(&cinfo);
+		free(buffer);
+		fclose(imageFile);
+		imageFile = NULL;
+
+		return true;
 	}
 
 	if( StrStrI(szFilename, TEXT(".png")) )
 	{
-		//TODO: PNG HERE PLSKTHX
 		std::vector<unsigned char> buffer;
 		std::ifstream file(szFilename, std::ios::in|std::ios::binary|std::ios::ate);
 		if(!file.is_open())
@@ -282,7 +280,9 @@ bool CAlbumArt::Draw(HDC hDC, long x, long y, long lWidth, long lHeight)
 	bmi.bmiHeader.biCompression	= BI_RGB;
 	bmi.bmiHeader.biSizeImage	= 0;
 
-	SetStretchBltMode(hDC, COLORONCOLOR);
+	//SetStretchBltMode(hDC, COLORONCOLOR);
+	SetStretchBltMode(hDC, HALFTONE);
+	SetBrushOrgEx(hDC, 0, 0, NULL);
 	int iRet = StretchDIBits(	hDC,
 								x,
 								y, 
