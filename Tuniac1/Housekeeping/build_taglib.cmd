e734f6644be60d255c81dd5e4837e98e74ac3de7
twistedddx <twistedddx@164ec1c6-e1a9-4765-950d-f2db5f2bc275>
2012-03-08 08:19:15 +0000

    -static zlib library

--- Tuniac1/Housekeeping/build_taglib.cmd
+++ Tuniac1/Housekeeping/build_taglib.cmd
@@ -5,10 +5,9 @@ cd zlib\
 
 rem #Release x86
 del ".\CMakeCache.txt"
-
 call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /Release /x86 /win7
 call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin\vcvars32.bat"
-"C:\Program Files (x86)\CMake 2.8\bin\cmake" -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release .
+"C:\Program Files (x86)\CMake 2.8\bin\cmake" -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF .
 nmake clean
 nmake
 del zlib32.lib
@@ -18,7 +17,7 @@ rem #Release x64
 del ".\CMakeCache.txt"
 call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /Release /x64 /win7
 call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin\x86_amd64\vcvarsx86_amd64.bat"
-"C:\Program Files (x86)\CMake 2.8\bin\cmake" -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release .
+"C:\Program Files (x86)\CMake 2.8\bin\cmake" -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF .
 nmake clean
 nmake
 del zlib64.lib
