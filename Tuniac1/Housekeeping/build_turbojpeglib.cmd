0b9ef7a701d496500df38fe0717f49afa09a5a6c
twistedddx <twistedddx@users.noreply.github.com>
2012-03-24 05:58:45 +0000

    -working detection of 64bit windows and 64bit compiler. -remove some extra quotations marks breaking things

--- Tuniac1/Housekeeping/build_turbojpeglib.cmd
+++ Tuniac1/Housekeeping/build_turbojpeglib.cmd
@@ -1,13 +1,15 @@
 cd ..\..\..\
 
-set 32bitonly=false
 set ProgFilesRoot=%ProgramFiles%
 set ProgFiles86Root=%ProgramFiles(x86)%
-if not "%ProgFiles86Root%"=="" GOTO 64bit
+if not "%ProgFiles86Root%"=="" goto 64bitWindows
 set ProgFiles86Root=%ProgramFiles%
-set 32bitonly=true
-:64bit
+:64bitWindows
 
+set 64bitcl=true
+if not exist "%ProgFiles86Root%\Microsoft Visual Studio 10.0\VC\bin\amd64\cl.exe" goto 32bitcl
+set 64bitcl=false
+:32bitcl
 
 rem ######## turbojpeglib
 cd libjpeg-turbo\trunk\
@@ -15,20 +17,20 @@ cd libjpeg-turbo\trunk\
 rem #Release x86:
 del ".\CMakeCache.txt"
 call "%ProgFilesRoot%\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /Release /x86 /win7
-call "%ProgFiles86Root%"\Microsoft Visual Studio 10.0\VC\bin\vcvars32.bat"
-"%ProgFiles86Root%"\CMake 2.8\bin\cmake" -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DWITH_JPEG8=1 .
+call "%ProgFiles86Root%\Microsoft Visual Studio 10.0\VC\bin\vcvars32.bat"
+"%ProgFiles86Root%\CMake 2.8\bin\cmake" -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DWITH_JPEG8=1 .
 nmake clean
 nmake
 mkdir .\Release\x86
 move /Y turbojpeg-static.lib .\Release\x86\turbojpeg-static.lib
 
-if "32bitonly"=="true" goto done
+if "64bitcl"=="false" goto done
 
 rem #Release x64:
 del ".\CMakeCache.txt"
 call "%ProgFilesRoot%\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /Release /x64 /win7
-call "%ProgFiles86Root%"\Microsoft Visual Studio 10.0\VC\bin\x86_amd64\vcvarsx86_amd64.bat"
-"%ProgFiles86Root%"\CMake 2.8\bin\cmake" -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DWITH_JPEG8=1 .
+call "%ProgFiles86Root%\Microsoft Visual Studio 10.0\VC\bin\x86_amd64\vcvarsx86_amd64.bat"
+"%ProgFiles86Root%\CMake 2.8\bin\cmake" -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DWITH_JPEG8=1 .
 nmake clean
 nmake
 mkdir .\Release\x64
