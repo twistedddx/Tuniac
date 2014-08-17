b5273b8ed0c99b8bf37e11f2e4793ce18f281bae
twistedddx <twistedddx@users.noreply.github.com>
2014-08-17 15:20:31 +0000

    -Update basslib dll's -update external lib build scripts to use out of source building. Fixes x64 build.

--- Tuniac1/Housekeeping/build_libpng.cmd
+++ Tuniac1/Housekeeping/build_libpng.cmd
@@ -10,47 +10,48 @@ set 64bitcl=false
 :64bitcl
 
 
-call "%VS120COMNTOOLS%VsDevCmd.bat"
 call "%Programfiles(x86)%\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
 
 cd ..\..\..\
 
-rem ######## turbojpeglib
+rem ######## libpng
 cd libpng\
+mkdir .\build
+cd build\
 
 rem #Release x86:
-del ".\CMakeCache.txt"
-"%ProgFiles86Root%\CMake\bin\cmake" -DPNG_STATIC=1 -DBUILD_SHARED_LIBS=OFF -DZLIB_INCLUDE_DIR=..\zlib\ -DZLIB_LIBRARY=..\zlib\Release\x86\zlibstatic.lib -G "Visual Studio 12" .
+rmdir /S /Q ..\build
+"%ProgFiles86Root%\CMake\bin\cmake" -DPNG_STATIC=1 -DBUILD_SHARED_LIBS=OFF -DZLIB_INCLUDE_DIR=..\..\zlib\ -DZLIB_LIBRARY=..\..\zlib\Release\x86\zlibstatic.lib -G "Visual Studio 12" ..\.
 devenv libpng.sln /project "png17_static" /Clean
 devenv libpng.sln /project "png17_static" /Rebuild "Release"
-mkdir .\Release\x86
-move /Y .\Release\libpng17_static.lib .\Release\x86\libpng17_static.lib
+mkdir ..\Release\x86
+move /Y .\Release\libpng17_static.lib ..\Release\x86\libpng17_static.lib
 
 rem #Debug x86:
 devenv libpng.sln /project "png17_static" /Clean
 devenv libpng.sln /project "png17_static" /Rebuild "Debug"
-mkdir .\Debug\x86
-move /Y .\Debug\libpng17_staticd.lib .\Debug\x86\libpng17_staticd.lib
-move /Y .\Debug\libpng17_staticd.pdb .\Debug\x86\libpng17_staticd.pdb
+mkdir ..\Debug\x86
+move /Y .\Debug\libpng17_staticd.lib ..\Debug\x86\libpng17_staticd.lib
+move /Y .\Debug\libpng17_staticd.pdb ..\Debug\x86\libpng17_staticd.pdb
 
 rem if "64bitcl"=="false" goto done
-call "%Programfiles(x86)%\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x64
+call "%Programfiles(x86)%\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64
 
 
 rem #Release x64:
-del ".\CMakeCache.txt"
-"%ProgFiles86Root%\CMake\bin\cmake" -DPNG_STATIC=1 -DBUILD_SHARED_LIBS=OFF -DZLIB_INCLUDE_DIR=..\zlib\ -DZLIB_LIBRARY=..\zlib\Release\x64\zlibstatic.lib -G "Visual Studio 12 Win64" .
+rmdir /S /Q ..\build
+"%ProgFiles86Root%\CMake\bin\cmake" -DPNG_STATIC=1 -DBUILD_SHARED_LIBS=OFF -DZLIB_INCLUDE_DIR=..\..\zlib\ -DZLIB_LIBRARY=..\..\zlib\Release\x64\zlibstatic.lib -G "Visual Studio 12 Win64" ..\.
 devenv libpng.sln /project "png17_static" /Clean
 devenv libpng.sln /project "png17_static" /Rebuild "Release"
-mkdir .\Release\x64
-move /Y .\Release\libpng17_static.lib .\Release\x64\libpng17_static.lib
+mkdir ..\Release\x64
+move /Y .\Release\libpng17_static.lib ..\Release\x64\libpng17_static.lib
 
 rem #Debug x64:
 devenv libpng.sln /project "png17_static" /Clean
 devenv libpng.sln /project "png17_static" /Rebuild "Debug"
-mkdir .\Debug\x64
-move /Y .\Debug\libpng17_staticd.lib .\Debug\x64\libpng17_staticd.lib
-move /Y .\Debug\libpng17_staticd.pdb .\Debug\x64\libpng17_staticd.pdb
+mkdir ..\Debug\x64
+move /Y .\Debug\libpng17_staticd.lib ..\Debug\x64\libpng17_staticd.lib
+move /Y .\Debug\libpng17_staticd.pdb ..\Debug\x64\libpng17_staticd.pdb
 
 :done
 
--- Tuniac1/Housekeeping/build_turbojpeglib.cmd
+++ Tuniac1/Housekeeping/build_turbojpeglib.cmd
@@ -10,34 +10,35 @@ set 64bitcl=false
 :64bitcl
 
 
-call "%VS120COMNTOOLS%VsDevCmd.bat"
 call "%Programfiles(x86)%\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
 
 cd ..\..\..\
 
 rem ######## turbojpeglib
 cd libjpeg-turbo\
+mkdir .\build
+cd build\
 
-rem #Release x86:
-del ".\CMakeCache.txt"
-"%ProgFiles86Root%\CMake\bin\cmake" -DCMAKE_BUILD_TYPE=Release -DWITH_JPEG8=1 -G "Visual Studio 12" .
+rem #turbojpeg Release x86:
+rmdir /S /Q ..\build
+"%ProgFiles86Root%\CMake\bin\cmake" -DCMAKE_BUILD_TYPE=Release -DWITH_JPEG8=1 -G "Visual Studio 12" ..\.
 devenv libjpeg-turbo.sln /project "turbojpeg-static" /Clean
 devenv libjpeg-turbo.sln /project "turbojpeg-static" /Rebuild "Release"
-mkdir .\Release\x86
-move /Y .\Release\turbojpeg-static.lib .\Release\x86\turbojpeg-static.lib
+mkdir ..\Release\x86
+move /Y .\Release\turbojpeg-static.lib ..\Release\x86\turbojpeg-static.lib
 
 
 rem if "64bitcl"=="false" goto done
-call "%Programfiles(x86)%\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x64
+call "%Programfiles(x86)%\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64
 
 
-rem #Release x64:
-del ".\CMakeCache.txt"
-"%ProgFiles86Root%\CMake\bin\cmake" -DCMAKE_BUILD_TYPE=Release -DWITH_JPEG8=1 -G "Visual Studio 12 Win64" .
+rem #turbojpeg Release x64
+rmdir /S /Q ..\build
+"%ProgFiles86Root%\CMake\bin\cmake" -DCMAKE_BUILD_TYPE=Release -DWITH_JPEG8=1 -G "Visual Studio 12 Win64" ..\.
 devenv libjpeg-turbo.sln /Project "turbojpeg-static" /Clean
 devenv libjpeg-turbo.sln /Project "turbojpeg-static" /Rebuild "Release"
-mkdir .\Release\x64
-move /Y .\Release\turbojpeg-static.lib .\Release\x64\turbojpeg-static.lib
+mkdir ..\Release\x64
+move /Y .\Release\turbojpeg-static.lib ..\Release\x64\turbojpeg-static.lib
 
 :done
 
