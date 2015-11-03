e07297a9935c75475926be205d8f322b1d06807c
twistedddx <twistedddx@164ec1c6-e1a9-4765-950d-f2db5f2bc275>
2015-11-03 12:25:22 +0000

    -nicer build scripts for external (removes a few pointless error messages and cleans dirs better)

--- Tuniac1/Housekeeping/build_libpng.cmd
+++ Tuniac1/Housekeeping/build_libpng.cmd
@@ -16,42 +16,50 @@ cd ..\..\..\
 
 rem ######## libpng
 cd libpng\
-mkdir .\build
-cd build\
 
 rem #Release x86:
-rmdir /S /Q ..\build
+if exist .\build\ rmdir /S /Q .\build
+mkdir .\build
+cd build\
 "%ProgFiles86Root%\CMake\bin\cmake" -DPNG_STATIC=1 -DBUILD_SHARED_LIBS=OFF -DZLIB_INCLUDE_DIR=..\..\zlib\ -DZLIB_LIBRARY=..\..\zlib\Release\x86\zlibstatic.lib -G "Visual Studio 14" ..\.
 devenv libpng.sln /project "png17_static" /Clean
 devenv libpng.sln /project "png17_static" /Rebuild "Release"
+if exist ..\Release\x86\ rmdir /S /Q ..\Release\x86
 mkdir ..\Release\x86
 move /Y .\Release\libpng17_static.lib ..\Release\x86\libpng17_static.lib
 
 rem #Debug x86:
 devenv libpng.sln /project "png17_static" /Clean
 devenv libpng.sln /project "png17_static" /Rebuild "Debug"
+if exist ..\Debug\x86\ rmdir /S /Q ..\Debug\x86
 mkdir ..\Debug\x86
 move /Y .\Debug\libpng17_staticd.lib ..\Debug\x86\libpng17_staticd.lib
-move /Y .\Debug\libpng17_staticd.pdb ..\Debug\x86\libpng17_staticd.pdb
+
+
+cd ..
+
 
 rem if "64bitcl"=="false" goto done
 call "%Programfiles(x86)%\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 
 
 rem #Release x64:
-rmdir /S /Q ..\build
+if exist .\build\ rmdir /S /Q .\build
+mkdir .\build
+cd build\
 "%ProgFiles86Root%\CMake\bin\cmake" -DPNG_STATIC=1 -DBUILD_SHARED_LIBS=OFF -DZLIB_INCLUDE_DIR=..\..\zlib\ -DZLIB_LIBRARY=..\..\zlib\Release\x64\zlibstatic.lib -G "Visual Studio 14 Win64" ..\.
 devenv libpng.sln /project "png17_static" /Clean
 devenv libpng.sln /project "png17_static" /Rebuild "Release"
+if exist ..\Release\x64\ rmdir /S /Q ..\Release\x64
 mkdir ..\Release\x64
 move /Y .\Release\libpng17_static.lib ..\Release\x64\libpng17_static.lib
 
 rem #Debug x64:
 devenv libpng.sln /project "png17_static" /Clean
 devenv libpng.sln /project "png17_static" /Rebuild "Debug"
+if exist ..\Debug\x64\ rmdir /S /Q ..\Debug\x64
 mkdir ..\Debug\x64
 move /Y .\Debug\libpng17_staticd.lib ..\Debug\x64\libpng17_staticd.lib
-move /Y .\Debug\libpng17_staticd.pdb ..\Debug\x64\libpng17_staticd.pdb
 
 move /Y .\pnglibconf.h ..\pnglibconf.h
 
--- Tuniac1/Housekeeping/build_taglib.cmd
+++ Tuniac1/Housekeeping/build_taglib.cmd
@@ -16,23 +16,28 @@ cd ..\..\..\
 
 rem ######## Taglib
 cd taglib\
-mkdir .\build
-cd build\
 
 rem #taglib Release x86
-rmdir /S /Q ..\build
+if exist .\build\ rmdir /S /Q .\build
+mkdir .\build
+cd build\
 "%ProgFiles86Root%\CMake\bin\cmake" -DENABLE_STATIC=1 -DBUILD_SHARED_LIBS=OFF -DZLIB_INCLUDE_DIR=..\..\zlib\ -DZLIB_LIBRARY=..\..\zlib\Release\x86\zlibstatic.lib -G "Visual Studio 14" ..\.
 devenv taglib.sln /project "tag" /Clean
 devenv taglib.sln /project "tag" /Rebuild "Release"
+if exist ..\taglib\Release\x86\ rmdir /S /Q ..\taglib\Release\x86
 mkdir ..\taglib\Release\x86
 move /Y .\taglib\Release\tag.lib ..\taglib\Release\x86\tag.lib
 
 rem #taglib Debug x86
 devenv taglib.sln /project "tag" /Clean
 devenv taglib.sln /project "tag" /Rebuild "Debug"
+if exist ..\taglib\Debug\x86\ rmdir /S /Q ..\taglib\Debug\x86
 mkdir ..\taglib\Debug\x86
 move /Y .\taglib\Debug\tag.lib ..\taglib\Debug\x86\tag.lib
 move /Y .\taglib\Debug\tag.pdb ..\taglib\Debug\x86\tag.pdb
+
+
+cd ..
 
 
 rem if "64bitcl"=="false" goto done
@@ -43,16 +48,20 @@ rem ######## Taglib x64
 
 
 rem #taglib Release x64
-rmdir /S /Q ..\build
+if exist .\build\ rmdir /S /Q .\build
+mkdir .\build
+cd build\
 "%ProgFiles86Root%\CMake\bin\cmake" -DENABLE_STATIC=1 -DBUILD_SHARED_LIBS=OFF -DZLIB_INCLUDE_DIR=..\..\zlib\ -DZLIB_LIBRARY=..\..\zlib\Release\x64\zlibstatic.lib -G "Visual Studio 14 Win64" ..\.
 devenv taglib.sln /project "tag" /Clean
 devenv taglib.sln /project "tag" /Rebuild "Release"
+if exist ..\taglib\Release\x64\ rmdir /S /Q ..\taglib\Release\x64
 mkdir ..\taglib\Release\x64
 move /Y .\taglib\Release\tag.lib ..\taglib\Release\x64\tag.lib
 
 rem #taglib Debug x64
 devenv taglib.sln /project "tag" /Clean
 devenv taglib.sln /project "tag" /Rebuild "Debug"
+if exist ..\taglib\Debug\x64\ rmdir /S /Q ..\taglib\Debug\x64
 mkdir ..\taglib\Debug\x64
 move /Y .\taglib\Debug\tag.lib ..\taglib\Debug\x64\tag.lib
 move /Y .\taglib\Debug\tag.pdb ..\taglib\Debug\x64\tag.pdb
