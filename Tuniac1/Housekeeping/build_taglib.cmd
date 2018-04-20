16d0ad0c1bcad8d55257d2284a2bae39bb402ea0
twistedddx <twistedddx@users.noreply.github.com>
2018-04-20 13:16:51 +0000

    -remove 32bit build machine checks in external lib build scripts

--- Tuniac1/Housekeeping/build_taglib.cmd
+++ Tuniac1/Housekeeping/build_taglib.cmd
@@ -1,15 +1,3 @@
-set ProgFilesRoot=%ProgramFiles%
-set ProgFiles86Root=%ProgramFiles(x86)%
-if not "%ProgFiles86Root%"=="" goto 64bitWindows
-set ProgFiles86Root=%ProgramFiles%
-:64bitWindows
-
-set 64bitcl=true
-if exist "%ProgFiles86Root%\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.10.24911\bin\HostX86\x86\cl.exe" goto 64bitcl
-set 64bitcl=false
-:64bitcl
-
-
 call "%Programfiles(x86)%\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
 
 cd ..\..\..\
@@ -17,11 +5,12 @@ cd ..\..\..\
 rem ######## Taglib
 cd taglib\
 
+
 rem #taglib Release x86
 rmdir /s /q .\build32
 mkdir .\build32
 cd build32\
-"%ProgFilesRoot%\CMake\bin\cmake" -DBUILD_SHARED_LIBS=OFF -DZLIB_INCLUDE_DIR=..\..\zlib\ -DZLIB_LIBRARY=..\..\zlib\Release\x86\zlibstatic.lib -G "Visual Studio 15" ..\.
+"%ProgramFiles%\CMake\bin\cmake" -DBUILD_SHARED_LIBS=OFF -DZLIB_INCLUDE_DIR=..\..\zlib\ -DZLIB_LIBRARY=..\..\zlib\Release\x86\zlibstatic.lib -G "Visual Studio 15" ..\.
 devenv taglib.sln /project "tag" /Clean
 devenv taglib.sln /project "tag" /Rebuild "Release"
 if exist ..\taglib\Release\x86\ rmdir /S /Q ..\taglib\Release\x86
@@ -40,7 +29,6 @@ move /Y .\taglib\Debug\tag.pdb ..\taglib\Debug\x86\tag.pdb
 cd ..
 
 
-rem if "64bitcl"=="false" goto done
 call "%Programfiles(x86)%\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
 
 
@@ -51,7 +39,7 @@ rem #taglib Release x64
 rmdir /s /q .\build64
 mkdir .\build64
 cd build64\
-"%ProgFilesRoot%\CMake\bin\cmake" -DBUILD_SHARED_LIBS=OFF -DZLIB_INCLUDE_DIR=..\..\zlib\ -DZLIB_LIBRARY=..\..\zlib\Release\x64\zlibstatic.lib -G "Visual Studio 15 Win64" ..\.
+"%ProgramFiles%\CMake\bin\cmake" -DBUILD_SHARED_LIBS=OFF -DZLIB_INCLUDE_DIR=..\..\zlib\ -DZLIB_LIBRARY=..\..\zlib\Release\x64\zlibstatic.lib -G "Visual Studio 15 Win64" ..\.
 devenv taglib.sln /project "tag" /Clean
 devenv taglib.sln /project "tag" /Rebuild "Release"
 if exist ..\taglib\Release\x64\ rmdir /S /Q ..\taglib\Release\x64
@@ -68,6 +56,5 @@ move /Y .\taglib\Debug\tag.pdb ..\taglib\Debug\x64\tag.pdb
 
 move /Y .\taglib_config.h ..\taglib_config.h
 
-:done 
 
 pause
--- Tuniac1/Housekeeping/build_turbojpeglib.cmd
+++ Tuniac1/Housekeeping/build_turbojpeglib.cmd
@@ -1,15 +1,3 @@
-set ProgFilesRoot=%ProgramFiles%
-set ProgFiles86Root=%ProgramFiles(x86)%
-if not "%ProgFiles86Root%"=="" goto 64bitWindows
-set ProgFiles86Root=%ProgramFiles%
-:64bitWindows
-
-set 64bitcl=true
-if exist "%ProgFiles86Root%\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.10.24911\bin\HostX86\x86\cl.exe" goto 64bitcl
-set 64bitcl=false
-:64bitcl
-
-
 call "%Programfiles(x86)%\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
 
 cd ..\..\..\
@@ -22,7 +10,7 @@ rem #turbojpeg Release x86:
 rmdir /s /q .\build32
 mkdir .\build32
 cd build32\
-"%ProgFilesRoot%\CMake\bin\cmake" -DCMAKE_BUILD_TYPE=Release -DWITH_JPEG8=1 -G "Visual Studio 15" ..\.
+"%ProgramFiles%\CMake\bin\cmake" -DCMAKE_BUILD_TYPE=Release -DWITH_JPEG8=1 -G "Visual Studio 15" ..\.
 devenv libjpeg-turbo.sln /project "jpeg-static" /Clean
 devenv libjpeg-turbo.sln /project "jpeg-static" /Rebuild "Release"
 if exist ..\Release\x86\ rmdir /S /Q ..\Release\x86
@@ -33,7 +21,6 @@ move /Y .\Release\jpeg-static.lib ..\Release\x86\jpeg-static.lib
 cd ..
 
 
-rem if "64bitcl"=="false" goto done
 call "%Programfiles(x86)%\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
 
 
@@ -41,7 +28,7 @@ rem #turbojpeg Release x64
 rmdir /s /q .\build64
 mkdir .\build64
 cd build64\
-"%ProgFilesRoot%\CMake\bin\cmake" -DCMAKE_BUILD_TYPE=Release -DWITH_JPEG8=1 -G "Visual Studio 15 Win64" ..\.
+"%ProgramFiles%\CMake\bin\cmake" -DCMAKE_BUILD_TYPE=Release -DWITH_JPEG8=1 -G "Visual Studio 15 Win64" ..\.
 devenv libjpeg-turbo.sln /Project "jpeg-static" /Clean
 devenv libjpeg-turbo.sln /Project "jpeg-static" /Rebuild "Release"
 if exist ..\Release\x64\ rmdir /S /Q ..\Release\x64
@@ -50,6 +37,5 @@ move /Y .\Release\jpeg-static.lib ..\Release\x64\jpeg-static.lib
 
 move /Y .\jconfig.h ..\jconfig.h
 
-:done
 
 pause
