a1051ddd79e69bad8c20be95e06472ca1ee99106
twistedddx <twistedddx@users.noreply.github.com>
2022-01-05 00:11:27 +0000

    basslib update

--- Tuniac1/Housekeeping/build_libpng.cmd
+++ Tuniac1/Housekeeping/build_libpng.cmd
@@ -1,4 +1,4 @@
-call "%Programfiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
+call "%Programfiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
 
 echo Working dir: %cd%
 
@@ -14,7 +14,7 @@ rmdir /s /q .\build32
 mkdir .\build32
 cd build32\
 echo Working dir: %cd%
-"%ProgramFiles%\CMake\bin\cmake" -DPNG_STATIC=1 -DBUILD_SHARED_LIBS=OFF -DZLIB_INCLUDE_DIR=..\..\zlib\ -DZLIB_LIBRARY=..\..\zlib\Release\x86\zlibstatic.lib -G "Visual Studio 16 2019" -A Win32 ..\.
+"%ProgramFiles%\CMake\bin\cmake" -DPNG_STATIC=1 -DBUILD_SHARED_LIBS=OFF -DZLIB_INCLUDE_DIR=..\..\zlib\ -DZLIB_LIBRARY=..\..\zlib\Release\x86\zlibstatic.lib -G "Visual Studio 17 2022" -A Win32 ..\.
 devenv libpng.sln /project "png_static" /Clean
 devenv libpng.sln /project "png_static" /Rebuild "Release"
 if exist ..\Release\x86\ rmdir /S /Q ..\Release\x86
@@ -37,7 +37,7 @@ move /Y .\png_static.dir\Debug\png_static.pdb ..\Debug\x86\png_static.pdb
 cd ..
 echo Working dir: %cd%
 
-call "%Programfiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
+call "%Programfiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
 
 
 rem #Release x64:
@@ -45,7 +45,7 @@ rmdir /s /q .\build64
 mkdir .\build64
 cd build64\
 echo Working dir: %cd%
-"%ProgramFiles%\CMake\bin\cmake" -DPNG_STATIC=1 -DBUILD_SHARED_LIBS=OFF -DZLIB_INCLUDE_DIR=..\..\zlib\ -DZLIB_LIBRARY=..\..\zlib\Release\x64\zlibstatic.lib -G "Visual Studio 16 2019" -A x64 ..\.
+"%ProgramFiles%\CMake\bin\cmake" -DPNG_STATIC=1 -DBUILD_SHARED_LIBS=OFF -DZLIB_INCLUDE_DIR=..\..\zlib\ -DZLIB_LIBRARY=..\..\zlib\Release\x64\zlibstatic.lib -G "Visual Studio 17 2022" -A x64 ..\.
 devenv libpng.sln /project "png_static" /Clean
 devenv libpng.sln /project "png_static" /Rebuild "Release"
 if exist ..\Release\x64\ rmdir /S /Q ..\Release\x64
