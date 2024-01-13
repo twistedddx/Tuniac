4a77b1589b2af507d1092a19359ee04da447e816
Brett H <bits@live.com>
2024-01-13 20:03:41 +1030

    Update vcpkg script

--- Tuniac1/Housekeeping/build_vcpkg.cmd
+++ Tuniac1/Housekeeping/build_vcpkg.cmd
@@ -5,21 +5,27 @@ echo Working dir: %cd%
 
 if exist vcpkg GOTO SKIPINSTALL
 
-rem ## Install
+rem ## Install vcpkg
 call git clone https://github.com/Microsoft/vcpkg.git
 cd vcpkg
 call .\bootstrap-vcpkg.bat
 .\vcpkg.exe integrate install
+cd..
 
+
+rem ## vcpkg install zlib utf8cpp
 :SKIPINSTALL
+cd vcpkg
+
 if exist .\installed\x86-windows\include\utf8cpp GOTO UPGRADE
 
 .\vcpkg.exe install utfcpp:x86-windows zlib:x86-windows
 .\vcpkg.exe install utfcpp:x64-windows zlib:x64-windows
+ cd..
 
 GOTO END
 
-rem ## Upgrade
+rem ## vcpkg upgrade zlib utf8cpp
 :UPGRADE
 cd vcpkg
 .\vcpkg.exe upgrade
